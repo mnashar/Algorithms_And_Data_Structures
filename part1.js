ec14e2b9cdad28f394ccd1d726e59753bc51764c
your name here <msn@aucegypt.edu>
2020-02-14 11:23:01 -0500

    Algorithms

--- part1.js
+++ part1.js
@@ -109,3 +109,8 @@ function titleize(names, callback) {
     let titleized = names.map(name => `Mx. ${name} Jingleheimer Schmidt`);
     callback(titleized);
 };
+
+// invoking the function
+titleize(["Mary", "Brian", "Leo"], (names) => {
+    names.forEach(name => console.log(name));
+});
