731c23f5a0693418e0b11c68f54815d4d2223048
your name here <msn@aucegypt.edu>
2020-02-14 09:02:54 -0500

    Algorithms

--- part1.js
+++ part1.js
@@ -72,3 +72,35 @@ function fizzBuzz(array) {
 
     return fizzBuzzArr;
 }
+
+// isPrime
+
+function isPrime(n) {
+    if (n < 2) { return false; }
+
+    for (let i = 2; i < n; i++) {
+        if (n % i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+// sumOfNPrimes
+
+function sumOfNPrimes(n) {
+    let sum = 0;
+    let countPrimes = 0;
+    let i = 2;
+
+    while (countPrimes < n) {
+        if (isPrime(i)) {
+            sum += i;
+            countPrimes++;
+        }
+        i++;
+    }
+
+    return sum;
+}
