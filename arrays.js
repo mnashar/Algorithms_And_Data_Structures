3be5e4c800b3ddfe83d12372c4725251432bcaec
your name here <msn@aucegypt.edu>
2020-02-19 12:09:24 -0500

    Algorithms

--- arrays.js
+++ arrays.js
@@ -25,3 +25,18 @@ Array.prototype.uniq2 = function () {
 
     return uniqueArray;
 };
+
+// Array#twoSum
+Array.prototype.twoSum = function () {
+    const pairs = [];
+
+    for (let i = 0; i < this.length; i++) {
+        for (let j = (i + 1); j < this.length; j++) {
+            if (this[i] + this[j] === 0) {
+                pairs.push([i, j]);
+            }
+        }
+    }
+
+    return pairs;
+};
