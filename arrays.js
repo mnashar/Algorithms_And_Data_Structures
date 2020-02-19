d6dfe17e311cd112451c66e6033c30e32e062f7e
your name here <msn@aucegypt.edu>
2020-02-19 13:22:17 -0500

    Algorithms

--- arrays.js
+++ arrays.js
@@ -60,3 +60,21 @@ Array.prototype.twoSum2 = function () {
 
     return pairs;
 };
+
+Array.prototype.transpose = function () {
+
+    // this creates the empty transposed array
+    // just a neat trick to avoid iterating
+    const columns = Array.from(
+        { length: this[0].length },
+        () => Array.from({ length: this.length })
+    );
+
+    for (let i = 0; i < this.length; i++) {
+        for (let j = 0; j < this[i].length; j++) {
+            columns[j][i] = this[i][j];
+        }
+    }
+
+    return columns;
+};
