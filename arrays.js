db027ce146381fe7633395a0b1dc3ff7a9779d87
your name here <msn@aucegypt.edu>
2020-02-19 12:45:31 -0500

    Algorithms

--- arrays.js
+++ arrays.js
@@ -40,3 +40,23 @@ Array.prototype.twoSum = function () {
 
     return pairs;
 };
+
+Array.prototype.twoSum2 = function () {
+    let pairs = [];
+    const indexHash = {};
+
+    this.forEach((el, idx) => {
+        if (indexHash[el * -1]) {
+            const newPairs = indexHash[el * -1].map((prevIdx) => [prevIdx, idx]);
+
+            // remember, concat doesn't mutate the original object
+            pairs = pairs.concat(newPairs);
+        }
+
+        // since we can't set a default attribute value in JavaScript,
+        // we will need to check for existence first
+        indexHash[el] ? indexHash[el].push(idx) : indexHash[el] = [idx];
+    });
+
+    return pairs;
+};
