fa9a33485c8ec42ea5606dae9003d832c7f2bbdc
your name here <msn@aucegypt.edu>
2020-02-19 11:43:25 -0500

    Algorithms

--- arrays.js
+++ arrays.js
@@ -10,3 +10,18 @@ Array.prototype.uniq = function () {
 
     return uniqueArray;
 };
+
+// alternate solution using 'forEach'
+Array.prototype.uniq2 = function () {
+    let uniqueArray = [];
+
+    // here we are using Array#forEach with a callback that is
+    // closing over 'uniqueArray'
+    this.forEach(function (el) {
+        if (!uniqueArray.includes(el)) {
+            uniqueArray.push(el);
+        }
+    });
+
+    return uniqueArray;
+};
