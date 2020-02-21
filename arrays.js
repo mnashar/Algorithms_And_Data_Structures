2558bc206523114c9e4b2f3f07261660e6b6a2d8
your name here <msn@aucegypt.edu>
2020-02-21 09:37:22 -0500

    Algorithms

--- arrays.js
+++ arrays.js
@@ -90,3 +90,12 @@ Array.prototype.myEach = function (func) {
 NUMS.myEach((num) => {
     console.log(`square of ${num} is ${num * num}`);
 });
+
+// Array#.myMap
+Array.prototype.myMap = function (func) {
+    const mappedArray = [];
+
+    this.myEach(element => mappedArray.push(func(element)));
+
+    return mappedArray;
+};
