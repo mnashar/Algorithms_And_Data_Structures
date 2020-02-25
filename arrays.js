9986ff6cd5f56f04db57672af469f867e5b47e60
your name here <msn@aucegypt.edu>
2020-02-25 09:47:19 -0500

    Algorithms

--- arrays.js
+++ arrays.js
@@ -124,7 +124,8 @@ Array.prototype.bubbleSort = function () {
 
         for (let i = 0; i < (this.length - 1); i++) {
             if (this[i] > this[i + 1]) {
-                
+                [this[i], this[i + 1]] = [this[i + 1], this[i]];
+                isSorted = false;
             }
         }
     }
