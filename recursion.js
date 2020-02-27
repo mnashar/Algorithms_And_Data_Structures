feb92e05ac31407fc65eaf166cc9c11cbeac5dbf
your name here <msn@aucegypt.edu>
2020-02-27 09:12:33 -0500

    Algorithms

--- recursion.js
+++ recursion.js
@@ -8,3 +8,13 @@ function range(start, end) {
     r.push(end - 1);
     return r;
 }
+
+// sumRec
+function sumRec(numbers) {
+    if (numbers.length === 0) {
+        return 0;
+    }
+
+    let lastNum = numbers[numbers.length - 1];
+    return sumRec(numbers.slice(0, numbers.length - 1)) + lastNum;
+}
