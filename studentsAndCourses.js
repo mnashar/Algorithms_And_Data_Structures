9b157b606aefd8691656a4f86a85b087f64b4e47
your name here <msn@aucegypt.edu>
2020-03-18 10:21:09 -0400

    Algorithms

--- studentsAndCourses.js
+++ studentsAndCourses.js
@@ -8,3 +8,16 @@ function Student(fname, lname) {
 Student.prototype.name = function () {
     return `${this.fname} ${this.lname}`;
 };
+
+Student.prototype.enroll = function (course) {
+    if (!this.courses.includes(course)) {
+        this.courses.forEach(crs => {
+            if (crs.conflictsWith(course)) {
+                throw "Course conflict";
+            }
+        });
+
+        this.courses.push(course);
+        course.addStudent(this);
+    }
+};
