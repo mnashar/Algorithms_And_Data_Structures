4f5e3aaf73252578591554c6658eb99be4316b5e
your name here <msn@aucegypt.edu>
2020-03-24 09:25:29 -0400

    Algorithms

--- bubbleSort.js
+++ bubbleSort.js
@@ -9,7 +9,11 @@ function askIfGreaterThan(el1, el2, callback) {
     reader.question(
         "Is " + el1 + " greater than " + el2 + "?: ",
         function (answer) {
-            
+            if (answer == "yes") {
+                callback(true);
+            } else {
+                callback(false);
+            }
         }
     );
 }
