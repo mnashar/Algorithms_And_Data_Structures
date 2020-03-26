adbfa0a03ad8e4e82e27046759f82da1dd6fd0c4
your name here <msn@aucegypt.edu>
2020-03-26 09:27:03 -0400

    Algorithms

--- addNumbers.js
+++ addNumbers.js
@@ -7,7 +7,14 @@ const reader = readline.createInterface({
 
 function addNumbers(sum, numsLeft, completionCallback) {
     if (numsLeft > 0) {
-        
+        reader.question("Give me num: ", function (numStr) {
+            const thisNumber = parseInt(numStr);
+
+            sum += thisNumber;
+            console.log("Partial sum: " + sum);
+
+            addNumbers(sum, numsLeft - 1, completionCallback);
+        });
     } else {
         
     }
