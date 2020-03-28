8fe09b573ca1703fcd2be3db802f1b1c2deff0ed
your name here <msn@aucegypt.edu>
2020-03-28 13:55:03 -0400

    Algorithms

--- clock.js
+++ clock.js
@@ -1,7 +1,18 @@
 class Clock {
     constructor() {
-        
-        
+        // 1. Create a Date object.
+        const currentTime = new Date();
+
+        // 2. Store the hour, minute, and second.
+        this.hours = currentTime.getHours();
+        this.minutes = currentTime.getMinutes();
+        this.seconds = currentTime.getSeconds();
+
+        // 3. Call printTime.
+        this.printTime();
+
+        // 4. Schedule the tick at 1 second intervals.
+        setInterval(this._tick.bind(this), 1000);
     }
 
 }
