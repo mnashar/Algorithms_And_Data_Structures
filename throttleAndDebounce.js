d7a557828bc94be2eaf312245a980536a88bbeff
your name here <msn@aucegypt.edu>
2020-03-30 09:44:22 -0400

    Algorithms

---
+++ throttleAndDebounce.js
@@ -0,0 +1,16 @@
+Function.prototype.myThrottle = function (interval) {
+    // declare a variable outside of the returned function
+    let tooSoon = false;
+    return (...args) => {
+        // the function only gets invoked if tooSoon is false
+        // it sets tooSoon to true, and uses setTimeout to set it back to
+        // false after interval ms
+        // any invocation within this interval will skip the if 
+        // statement and do nothing
+        if (!tooSoon) {
+            tooSoon = true;
+            setTimeout(() => tooSoon = false, interval);
+            this(...args);
+        }
+    }
+}
