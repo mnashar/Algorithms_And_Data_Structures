281a2408b31aea19746ca2d55ebf51b86119f921
your name here <msn@aucegypt.edu>
2020-04-03 09:43:11 -0400

    Algorithms

--- TowersOfHanoi/game.js
+++ TowersOfHanoi/game.js
@@ -3,7 +3,21 @@ class Game {
         this.towers = [[3, 2, 1], [], []];
     }
 
-  
+    isValidMove(startTowerIdx, endTowerIdx) {
+        const startTower = this.towers[startTowerIdx];
+        const endTower = this.towers[endTowerIdx];
+
+        if (startTower.length === 0) {
+            return false;
+        } else if (endTower.length == 0) {
+            return true;
+        } else {
+            const topStartDisc = startTower[startTower.length - 1];
+            const topEndDisc = endTower[endTower.length - 1];
+            return topStartDisc < topEndDisc;
+        }
+    }
+
 }
 
 module.exports = Game;
