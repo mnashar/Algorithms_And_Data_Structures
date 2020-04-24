235ce107f0a924b15221c76cfde9da0b6b10b8fa
your name here <msn@aucegypt.edu>
2020-04-24 09:13:55 -0400

    Algorithms

--- Tic-Tac-Toe/board.js
+++ Tic-Tac-Toe/board.js
@@ -5,9 +5,16 @@ class Board {
         this.grid = Board.makeGrid();
     }
 
-   
-}
+    isEmptyPos(pos) {
+        if (!Board.isValidPos(pos)) {
+            throw new MoveError('Is not valid position!');
+        }
 
+        return (this.grid[pos[0]][pos[1]] === null);
+    }
+
+    
+}
 
 
 module.exports = Board;
