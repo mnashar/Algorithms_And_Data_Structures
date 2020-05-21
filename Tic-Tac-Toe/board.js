7c99dc164167ce0db6b93cb70be36cea423ede0a
your name here <msn@aucegypt.edu>
2020-05-21 12:22:01 -0400

    Algorithms

--- Tic-Tac-Toe/board.js
+++ Tic-Tac-Toe/board.js
@@ -105,6 +105,18 @@ class Board {
             (pos[1] < 3);
     }
 
+    static makeGrid() {
+        const grid = [];
+
+        for (let i = 0; i < 3; i++) {
+            grid.push([]);
+            for (let j = 0; j < 3; j++) {
+                grid[i].push(null);
+            }
+        }
+
+        return grid;
+    }
 }
 
 
