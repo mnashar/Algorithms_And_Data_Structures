57650b7c30b12052141ec66c3be3a0bf3ecbb791
your name here <msn@aucegypt.edu>
2020-06-05 09:07:21 -0400

    Algorithms

--- Memoization/problems.js
+++ Memoization/problems.js
@@ -5,3 +5,25 @@ function lucasNumberMemo(n, memo = {}) {
     memo[n] = lucasNumberMemo(n - 1, memo) + lucasNumberMemo(n - 2, memo);
     return memo[n];
 }
+
+function minChange(coins, amount, memo = {}) {
+    if (amount === 0) return 0;
+
+    if (amount in memo) return memo[amount];
+
+    let num_coins = []
+    coins.forEach(coin => {
+        if (coin <= amount) {
+            num_coins.push(minChange(coins, amount - coin, memo) + 1);
+        }
+    });
+
+    memo[amount] = Math.min(...num_coins);
+    return memo[amount];
+}
+
+
+module.exports = {
+    lucasNumberMemo,
+    minChange
+};
