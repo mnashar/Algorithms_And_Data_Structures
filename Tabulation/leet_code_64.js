a46f9c3688e2bc256d02e791d31782f6fd3c6766
your name here <msn@aucegypt.edu>
2020-06-08 09:37:21 -0400

    Algorithms

--- Tabulation/leet_code_64.js
+++ Tabulation/leet_code_64.js
@@ -1,3 +1,8 @@
 function minPathSum(grid) {
-    
+    let m = grid.length;
+    let n = grid[0].length;
+    let table = new Array(m).fill().map(() => new Array(n).fill(Infinity));
+    table[0][0] = grid[0][0];
+
+   
 }
