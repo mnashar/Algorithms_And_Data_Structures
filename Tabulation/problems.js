560b11f6bafd139a1b07fd3aab70929f0271faac
your name here <msn@aucegypt.edu>
2020-06-12 13:27:44 -0400

    Algorithms

--- Tabulation/problems.js
+++ Tabulation/problems.js
@@ -13,3 +13,22 @@ function stepper(nums) {
 
     return table[table.length - 1];
 }
+
+function stepper(nums, memo = {}) {
+    if (nums.length in memo) return memo[nums.length];
+
+    if (nums.length === 1) return true;
+    if (nums.length === 0) return false;
+
+    let range = nums[0];
+
+    for (let i = 1; i <= range; i++) {
+        if (stepper(nums.slice(i), memo)) {
+            memo[nums.length] = true;
+            return true;
+        }
+    }
+
+    memo[nums.length] = false;
+    return false;
+}
