4dfba1dfee3a8d3ce573efe93ccadcb079b051b0
your name here <msn@aucegypt.edu>
2020-06-15 09:15:21 -0400

    Algorithms

--- Tabulation/problems.js
+++ Tabulation/problems.js
@@ -32,3 +32,20 @@ function stepper(nums, memo = {}) {
     memo[nums.length] = false;
     return false;
 }
+
+// tabulated
+function maxNonAdjacentSum(nums) {
+    if (nums.length === 0) return 0;
+
+    let table = new Array(nums.length).fill(0);
+    table[0] = nums[0];
+
+    for (let i = 1; i < table.length; i++) {
+        let skipLeftNeighbor = table[i - 2] === undefined ? 0 : table[i - 2];
+        let bestWithThisNum = nums[i] + skipLeftNeighbor;
+        let bestWithoutThisNum = table[i - 1];
+        table[i] = Math.max(bestWithThisNum, bestWithoutThisNum);
+    }
+
+    return table[table.length - 1];
+}
