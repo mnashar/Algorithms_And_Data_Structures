47ed16cf71279a6137377d14a5ded492d9fc99d8
your name here <msn@aucegypt.edu>
2020-06-22 15:43:28 -0400

    Algorithms

--- Tabulation/problems.js
+++ Tabulation/problems.js
@@ -77,3 +77,20 @@ function maxNonAdjacentSum(nums, memo = {}) {
 
     return memo[nums.length];
 }
+
+function minChange(coins, amount) {
+    let table = new Array(amount + 1).fill(Infinity);
+    table[0] = 0;
+
+    coins.forEach((val) => {
+        for (let amt = 0; amt < table.length; amt++) {
+            for (let qty = 0; qty * val <= amt; qty++) {
+                remainder = amt - qty * val;
+                attempt = table[remainder] + qty;
+                if (attempt < table[amt]) table[amt] = attempt;
+            }
+        }
+    });
+
+    return table[amount];
+}
