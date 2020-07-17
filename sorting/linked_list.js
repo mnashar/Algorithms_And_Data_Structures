e3ceb2b3aac54411b5233eb4be70832b28609792
your name here <msn@aucegypt.edu>
2020-07-17 10:11:49 -0400

    Algorithms

---
+++ sorting/linked_list.js
@@ -0,0 +1,15 @@
+class Node {
+    constructor(val) {
+        this.value = val;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    
