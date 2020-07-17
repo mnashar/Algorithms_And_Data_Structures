d9bcc3c6d1b1ec4d5bb1b652b954148a339c33de
your name here <msn@aucegypt.edu>
2020-07-17 11:25:21 -0400

    Algorithms

--- sorting/linked_list.js
+++ sorting/linked_list.js
@@ -97,6 +97,20 @@ class LinkedList {
         return false;
     }
 
+    insert(index, val) {
+        if (index < 0 || index >= this.length) return false;
+        if (index === this.length) return !!this.addToTail(val);
+        if (index === 0) return !!this.addToHead(val);
+
+        const newNode = new Node(val);
+        const prev = this.get(index - 1);
+        const temp = prev.next;
+        prev.next = newNode;
+        newNode.next = temp;
+        this.length++;
+        return true;
+    }
+
    
 }
 
