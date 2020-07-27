b54277141ec22a77b92d3ec162f0c12283e99b2e
your name here <msn@aucegypt.edu>
2020-07-27 09:17:31 -0400

    Algorithms

--- DataStructures/3_stack_queue.js
+++ DataStructures/3_stack_queue.js
@@ -14,6 +14,21 @@ class Stack {
         this.length = 0;
     }
 
-   
+    push(newStackNode) {
+        if (newStackNode.next) {
+            newStackNode.next = null;
+        }
+
+        if (!this.top) {
+            this.top = newStackNode;
+            this.bottom = newStackNode;
+        } else {
+            const temp = this.top;
+            this.top = newStackNode;
+            this.top.next = temp;
+        }
+        return ++this.length;
+    }
+
 };
 
