4e671ff2ca3d035af557dbd8d88ff438526d10af
your name here <msn@aucegypt.edu>
2020-08-11 09:47:00 -0400

    Algorithms

---
+++ Trie/trie.js
@@ -0,0 +1,15 @@
+class Node {
+    constructor() {
+        this.children = {};
+        this.isTerminal = false;
+    }
+}
+
+class Trie {
+    constructor() {
+        this.root = new Node();
+    }
+
+   
+}
+
