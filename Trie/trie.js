2de9d0b4ca0d9b8ad3bc0134aa6b3925f1265932
your name here <msn@aucegypt.edu>
2020-08-13 09:39:11 -0400

    Algorithms

--- Trie/trie.js
+++ Trie/trie.js
@@ -10,6 +10,20 @@ class Trie {
         this.root = new Node();
     }
 
+    insertRecur(word, root=this.root) {
+        let letter = word[0];
+        if (!(letter in root.children)) {
+            root.children[letter] = new Node();
+        }
+
+        if (word.length === 1) {
+            root.children[letter].isTerminal = true;
+        } else {
+            this.insertRecur(word.slice(1), root.children[letter]);
+        }
+    }
+
+    
     
 }
 
