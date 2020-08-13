4647c170823417f8d4db4c1fc053f6e1c19b2b36
your name here <msn@aucegypt.edu>
2020-08-13 10:51:44 -0400

    Algorithms

--- Trie/trie.js
+++ Trie/trie.js
@@ -72,6 +72,32 @@ class Trie {
         return node.isTerminal;
     }
 
-  
+    wordsWithPrefix(prefix, root=this.root) {
+        if (prefix.length === 0) {
+            let allWords = [];
+
+            if (root.isTerminal) allWords.push('');
+
+            for (let letter in root.children) {
+                let child = root.children[letter];
+                
+                let suffixes = this.wordsWithPrefix('', child);
+                let words = suffixes.map(word => letter + word);
+                allWords.push(...words);
+            }
+
+            return allWords;
+        } else {
+            let firstLetter = prefix[0];
+            let child = root.children[firstLetter];
+
+            if (child === undefined) {
+                return [];
+            } else {
+                let suffixes = this.wordsWithPrefix(prefix.slice(1), root.children[firstLetter]);
+                return suffixes.map(suffix => firstLetter + suffix);
+            }
+        }
+    }
 }
 
