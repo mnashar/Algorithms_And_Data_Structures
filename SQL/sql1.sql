801c6c24a5eb504702715181e327aa6af8bda208
your name here <msn@aucegypt.edu>
2020-08-18 09:54:12 -0400

    Algorithms

--- SQL/sql1.sql
+++ SQL/sql1.sql
@@ -6,3 +6,9 @@ from Employee
 group by team_id
 ) as teams
 on employee.team_id =teams.team_id
+
+-- https://leetcode.com/problems/last-person-to-fit-in-the-elevator/
+SELECT person_name 
+FROM (SELECT person_name, SUM(weight) OVER (ORDER BY turn) AS cumulative_weight FROM Queue) qc
+WHERE cumulative_weight <= 1000
+ORDER BY cumulative_weight DESC LIMIT 1;
