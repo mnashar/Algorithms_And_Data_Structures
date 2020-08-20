936c5932adacd00d421e0c18901442ec6f919b70
your name here <msn@aucegypt.edu>
2020-08-20 12:00:45 -0400

    Algorithms

--- SQL/sql1.sql
+++ SQL/sql1.sql
@@ -21,3 +21,13 @@ SELECT
 FROM scores
 GROUP BY gender, day
 ORDER BY gender, day;
+
+
+# Write your MySQL query statement below
+select 
+patient_id , patient_name , conditions 
+from
+Patients
+where
+conditions like '%DIAB1%'
+order by patient_id
