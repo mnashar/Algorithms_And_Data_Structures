bf1a0bb866e4e981a580d9aa6624a998392c99ad
your name here <msn@aucegypt.edu>
2020-08-24 10:56:01 -0400

    Algorithms

--- SQL/sql1.sql
+++ SQL/sql1.sql
@@ -66,5 +66,6 @@ and  product_name='S8';
 https://www.youtube.com/watch?v=KaPvDalVkZs
 https://www.youtube.com/watch?v=IVMfDpCGwK4
 https://www.youtube.com/watch?v=TzsrO4zTQj8
+https://www.youtube.com/watch?v=3Cw2fuO5TG0
 
 
